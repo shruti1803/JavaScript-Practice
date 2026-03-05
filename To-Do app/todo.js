c85d960d4201ab510c4734382babb21491ee816f
Shruti Sinha <94643549+shruti1803@users.noreply.github.com>
2026-03-05 06:32:30 +0000

    comments

--- To-Do app/todo.js
+++ To-Do app/todo.js
@@ -1,21 +1,37 @@
-const taskinput = document.getElementById("taskinput");
+// here document refers to the html doc, tskinput is the variable which contains the element taskinput from html
+// get element by id is used to access the element with the specified id in the html - here its taskinput
+const tskinput = document.getElementById("taskinput");
+
 const addtaskbtn = document.getElementById("addtaskbtn");
 const tasklist = document.getElementById("tasklist");
  
+// when button is clicked, function will be executed
 addtaskbtn.addEventListener("click", function () {
-    const task = taskinput.value;
+
+// task variable stores the value of tskinput 
+    const task = tskinput.value;
+
+// li element is created to make a list of tasks
     const li = document.createElement("li");
+
+// text content of li is set to the value of task
     li.textContent = task;
 
-    //delete button creation
+// delete button is created 
     const deletebtn = document.createElement("button");
+
+// will display the text delete on the button
     deletebtn.textContent = "Delete";
-    //delete button event listener
+
+// when delete button is clicked, the task will be removed from the list
     deletebtn.addEventListener("click", function() {
         tasklist.removeChild(li);
     });
 
+// delete button is added to the list
     li.appendChild(deletebtn);
+
+// list item is added to the tasklist
     tasklist.appendChild(li);
 });
 
